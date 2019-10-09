46293b7ab24f11266e68143fbc3132a14224c725
chokyungjin <chokj1472@gmail.com>
2019-10-09 18:10:56 +0900

    update

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/chokyungjin/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/chokyungjin/Documents/GitHub/flutter_airplane_mode_detection_/example"
-export "FLUTTER_TARGET=/Users/chokyungjin/Documents/GitHub/flutter_airplane_mode_detection_/example/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/chokyungjin/Documents/GitHub/flutter_airplane_mode_detection/example"
+export "FLUTTER_TARGET=/Users/chokyungjin/Documents/GitHub/flutter_airplane_mode_detection/example/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/chokyungjin/flutter/bin/cache/artifacts/engine/ios"
